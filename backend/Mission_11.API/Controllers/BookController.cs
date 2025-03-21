41bf6910ad8fbb4f6e4de7eeb6c10afc82150af8
Jordan Goddard <jordangoddard6@gmail.com>
2025-03-21 10:52:13 -0600

    DbContext and BookController set up

---
+++ backend/Mission_11.API/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission_11.API.Data;
+
+namespace Mission_11.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _bookContext;
+
+        public BookController(BookDbContext temp)
+        {
+            _bookContext = temp;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _bookContext.Books.ToList();
+        }
+    }
+}
